741435c25b252c8fa8e1ce132ee1d5cb2e900dae
emi kojima <emi8emi@gmail.com>
2018-02-28 00:10:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,7 @@ class Song
   end
   
   def self.create_from_filename
-  song = self.new
+  song = self.create
    new_file = file.chomp(".mp3").split(" - ")
    song.artist_name = new_file[0] 
    song.name = new_file[1] 
